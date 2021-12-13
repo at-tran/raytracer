426999404eb61a11099f4e1ca800ca9b4b15f71e
Anh Tuan Tran <nhanhnhanh17399@gmail.com>
2021-12-12 22:12:33 -0500

    Added sphere normal vector

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,25 @@ mod point;
 mod ray;
 mod vec3;
 
+fn hit_sphere(r: &Ray, center: &Point, radius: f64) -> f64 {
+    let oc = *r.origin() - *center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * r.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - f64::sqrt(discriminant)) / (2.0 * a)
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(r, &Point::new(0.0, 0.0, -1.0), 0.5);
+    if t > 0.0 {
+        let N = r.at(t) - Vec3::new(0.0, 0.0, -1.0);
+        return 0.5 * Color::new(N.0[0] + 1.0, N.0[1] + 1.0, N.0[2] + 1.0);
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction[1] + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
@@ -36,7 +54,10 @@ fn main() {
             let u = i as f64 / (image_width as f64 - 1.0);
             let v = j as f64 / (image_height as f64 - 1.0);
 
-            let r= Ray::new(origin, lower_left_corner - origin + u * horizontal + v * vertical);
+            let r = Ray::new(
+                origin,
+                lower_left_corner - origin + u * horizontal + v * vertical,
+            );
             let c = ray_color(&r);
 
             img_buf.put_pixel(i, image_height - j - 1, image::Rgb(c.to_rgb_array()));
