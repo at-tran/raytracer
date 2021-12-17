4b2626c79f57afe848e8bebdab799e5b5f4b2558
Anh Tuan Tran <nhanhnhanh17399@gmail.com>
2021-12-17 17:19:26 -0500

    Fuzzy metal reflections

--- src/material.rs
+++ src/material.rs
@@ -18,7 +18,7 @@ impl Lambertian {
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
+    fn scatter(&self, _: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
         let mut scatter_direction = rec.normal + Sphere::random_unit_vector();
 
         if scatter_direction.near_zero() {
@@ -31,19 +31,24 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self { albedo, fuzz: fuzz.min(1.0) }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
         let reflected = r_in.direction().unit_vector().reflect(&rec.normal);
-        if reflected.dot(&rec.normal) > 0.0 {
-            Some((Ray::new(rec.p, reflected), self.albedo))
+        let scattered = Ray::new(
+            rec.p,
+            reflected + self.fuzz * Sphere::random_in_unit_sphere().0,
+        );
+        if scattered.direction().dot(&rec.normal) > 0.0 {
+            Some((scattered, self.albedo))
         } else {
             None
         }
